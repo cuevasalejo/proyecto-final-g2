e5e31a95caa60165cc44ebda9693fa5151c72f1d
cuevasalejo <83356416+cuevasalejo@users.noreply.github.com>
2021-10-31 22:47:48 -0300

    Tarea 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors     = require('cors');
+const morgan   = require('morgan');
 
 const PORT = 3000;
 const DB   = 'mongodb://localhost/notasapp';
@@ -9,6 +11,10 @@ mongoose.connect(DB).then(() => console.log('DB conectada'));
 const app = express();
 // para que aparezca algo en req.body
 app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan('dev'));
 
 app.use('/', require('./api/note'));
 
