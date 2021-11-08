b9556ba06d16aecce3e4dd76a94c6fd9deaf2b6d
cuevasalejo <83356416+cuevasalejo@users.noreply.github.com>
2021-11-07 23:02:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors     = require('cors');
-const morgan   = require('morgan');
+const cors = require('cors');
 
-const PORT = 3000;
+const PORT = 4000;
 const DB   = 'mongodb://localhost/notasapp';
 
 mongoose.connect(DB).then(() => console.log('DB conectada'));
 
 const app = express();
+app.use(cors());
 // para que aparezca algo en req.body
 app.use(express.json());
-
-app.use(cors());
-
-app.use(morgan('dev'));
 
 app.use('/', require('./api/note'));
 
