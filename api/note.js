e8f4a672b96aa2b299691b985d081c3b7614af18
cuevasalejo <83356416+cuevasalejo@users.noreply.github.com>
2021-11-27 12:08:36 -0300

    Add files via upload

--- api/note.js
+++ api/note.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Note = require('../models/Note');
 
 router
+<<<<<<< HEAD
   .route('/api/notes')
   // buscar todas las notas
   .get((req, res) => {
@@ -40,3 +41,44 @@ router
   });
 
 module.exports = router;
+=======
+.route('/api/notes')
+.get((req, res) => {
+  Note.find((err, notes) => {
+    res.json(notes);
+  })
+})
+
+//crear una nota usando los datos de req.body
+.post((req, res)=> {
+  const note = new Note ({
+    title: req.body.title,
+    text:req.body.text
+  });
+  note.save((err, note) => {
+    res.json(note);
+  })
+});
+
+router
+.route('/api/notes/:id')
+//buscar una nota por id
+.get((req, res) => {
+  Note.find({ id : 1 }, (err, note) => {
+res.json(note);
+});
+})
+.put((req, res) => {
+  
+  Note.findByIdAndUpdate(id, note, options)
+  .exec((err,note)=>{
+
+  });
+})
+.delete((req, res) => {
+ Note.findByIdAndRemove(req.params.id, (err) => {
+   res.json({msg: 'Nota borrada'});
+ })
+});
+module.exports = router;
+>>>>>>> e169fe8 (Actualizacion)
