088822d7cd1dd93f80529250050058689f0c931c
cuevasalejo <83356416+cuevasalejo@users.noreply.github.com>
2021-11-27 12:07:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const PORT = 4000;
+<<<<<<< HEAD
 const DB   = 'mongodb://localhost/notasapp';
+=======
+const DB = 'mongodb://localhost/notasapp';
+>>>>>>> e169fe8 (Actualizacion)
 
 mongoose.connect(DB).then(() => console.log('DB conectada'));
 
 const app = express();
 app.use(cors());
+<<<<<<< HEAD
 // para que aparezca algo en req.body
 app.use(express.json());
 
@@ -17,3 +22,21 @@ app.use('/', require('./api/note'));
 app.use(express.static('public'));
 
 app.listen(PORT);
+=======
+//para que aparezca algo en req.body
+app.use(express.json());
+
+app.use('/',require ('./api/note'));
+
+// const User = require('./models/User');
+
+// app.get('/api/users', (req, res) => {
+  // User.find((err, users) => {
+  // res.json(users);
+  // });
+// });
+
+app.use(express.static('public'));
+
+app.listen(PORT);
+>>>>>>> e169fe8 (Actualizacion)
